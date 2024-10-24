cb85e75bd04ccce324325497360a2319d6aac608
Robert-California <evertonstyrer@gmail.com>
2024-10-24 10:01:34 +0200

    added more endpoints

--- CatalogService/API/CatalogApi.cs
+++ CatalogService/API/CatalogApi.cs
@@ -12,20 +12,17 @@ public static class CatalogApi
     {
         var api = app.MapGroup("api/catalog");
 
-        // Routes for querying catalog items.
         api.MapGet("/items", GetAllItems);
         api.MapGet("/items/by", GetItemsByIds);
         api.MapGet("/items/{id:int}", GetItemById);
         api.MapGet("/items/by/{name:minlength(1)}", GetItemsByName);
         //api.MapGet("/items/{catalogItemId:int}/pic", GetItemPictureById);
 
-        // Routes for resolving catalog items by type and brand.
-        //api.MapGet("/items/type/{typeId}/brand/{brandId?}", GetItemsByBrandAndTypeId);
-        //api.MapGet("/items/type/all/brand/{brandId:int?}", GetItemsByBrandId);
+        api.MapGet("/items/type/{typeId}/brand/{brandId?}", GetItemsByBrandAndTypeId);
+        api.MapGet("/items/type/all/brand/{brandId:int?}", GetItemsByBrandId);
         api.MapGet("/catalogtypes", async (CatalogContext context) => await context.CatalogTypes.OrderBy(x => x.Type).ToListAsync());
         api.MapGet("/catalogbrands", async (CatalogContext context) => await context.CatalogBrands.OrderBy(x => x.Brand).ToListAsync());
 
-        // Routes for modifying catalog items.
         //api.MapPut("/items", UpdateItem);
         //api.MapPost("/items", CreateItem);
         //api.MapDelete("/items/{id:int}", DeleteItemById);
@@ -100,80 +97,80 @@ public static class CatalogApi
         return TypedResults.Ok(new PaginatedItems<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage));
     }
 
-    /* public static async Task<Results<NotFound, PhysicalFileHttpResult>> GetItemPictureById(CatalogContext context, IWebHostEnvironment environment, int catalogItemId)
-     {
-         var item = await context.CatalogItems.FindAsync(catalogItemId);
+    /*public static async Task<Results<NotFound, PhysicalFileHttpResult>> GetItemPictureById(CatalogContext context, IWebHostEnvironment environment, int catalogItemId)
+    {
+        var item = await context.CatalogItems.FindAsync(catalogItemId);
 
-         if (item is null)
-         {
-             return TypedResults.NotFound();
-         }
+        if (item is null)
+        {
+            return TypedResults.NotFound();
+        }
 
-         var path = GetFullPath(environment.ContentRootPath, item.PictureFileName);
+        var path = GetFullPath(environment.ContentRootPath, item.PictureFileName);
 
-         string imageFileExtension = Path.GetExtension(item.PictureFileName);
-         string mimetype = GetImageMimeTypeFromImageFileExtension(imageFileExtension);
-         DateTime lastModified = File.GetLastWriteTimeUtc(path);
+        string imageFileExtension = Path.GetExtension(item.PictureFileName);
+        string mimetype = GetImageMimeTypeFromImageFileExtension(imageFileExtension);
+        DateTime lastModified = File.GetLastWriteTimeUtc(path);
 
-         return TypedResults.PhysicalFile(path, mimetype, lastModified: lastModified);
-     }
+        return TypedResults.PhysicalFile(path, mimetype, lastModified: lastModified);
+    }*/
 
 
 
-     public static async Task<Ok<PaginatedItems<CatalogItem>>> GetItemsByBrandAndTypeId(
-         [AsParameters] PaginationRequest paginationRequest,
-         [AsParameters] CatalogServices services,
-         int typeId,
-         int? brandId)
-     {
-         var pageSize = paginationRequest.PageSize;
-         var pageIndex = paginationRequest.PageIndex;
+    public static async Task<Ok<PaginatedItems<CatalogItem>>> GetItemsByBrandAndTypeId(
+        [AsParameters] PaginationRequest paginationRequest,
+        [AsParameters] CatalogServices services,
+        int typeId,
+        int? brandId)
+    {
+        var pageSize = paginationRequest.PageSize;
+        var pageIndex = paginationRequest.PageIndex;
 
-         var root = (IQueryable<CatalogItem>)services.Context.CatalogItems;
-         root = root.Where(c => c.CatalogTypeId == typeId);
-         if (brandId is not null)
-         {
-             root = root.Where(c => c.CatalogBrandId == brandId);
-         }
+        var root = (IQueryable<CatalogItem>)services.Context.CatalogItems;
+        root = root.Where(c => c.CatalogTypeId == typeId);
+        if (brandId is not null)
+        {
+            root = root.Where(c => c.CatalogBrandId == brandId);
+        }
 
-         var totalItems = await root
-             .LongCountAsync();
+        var totalItems = await root
+            .LongCountAsync();
 
-         var itemsOnPage = await root
-             .Skip(pageSize * pageIndex)
-             .Take(pageSize)
-             .ToListAsync();
+        var itemsOnPage = await root
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .ToListAsync();
 
-         return TypedResults.Ok(new PaginatedItems<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage));
-     }
+        return TypedResults.Ok(new PaginatedItems<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage));
+    }
 
-     public static async Task<Ok<PaginatedItems<CatalogItem>>> GetItemsByBrandId(
-         [AsParameters] PaginationRequest paginationRequest,
-         [AsParameters] CatalogServices services,
-         int? brandId)
-     {
-         var pageSize = paginationRequest.PageSize;
-         var pageIndex = paginationRequest.PageIndex;
+    public static async Task<Ok<PaginatedItems<CatalogItem>>> GetItemsByBrandId(
+        [AsParameters] PaginationRequest paginationRequest,
+        [AsParameters] CatalogServices services,
+        int? brandId)
+    {
+        var pageSize = paginationRequest.PageSize;
+        var pageIndex = paginationRequest.PageIndex;
 
-         var root = (IQueryable<CatalogItem>)services.Context.CatalogItems;
+        var root = (IQueryable<CatalogItem>)services.Context.CatalogItems;
 
-         if (brandId is not null)
-         {
-             root = root.Where(ci => ci.CatalogBrandId == brandId);
-         }
+        if (brandId is not null)
+        {
+            root = root.Where(ci => ci.CatalogBrandId == brandId);
+        }
 
-         var totalItems = await root
-             .LongCountAsync();
+        var totalItems = await root
+            .LongCountAsync();
 
-         var itemsOnPage = await root
-             .Skip(pageSize * pageIndex)
-             .Take(pageSize)
-             .ToListAsync();
+        var itemsOnPage = await root
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .ToListAsync();
 
-         return TypedResults.Ok(new PaginatedItems<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage));
-     }
+        return TypedResults.Ok(new PaginatedItems<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage));
+    }
 
-     public static async Task<Results<Created, NotFound<string>>> UpdateItem(
+    /* public static async Task<Results<Created, NotFound<string>>> UpdateItem(
          [AsParameters] CatalogServices services,
          CatalogItem productToUpdate)
      {
@@ -208,64 +205,64 @@ public static class CatalogApi
              await services.Context.SaveChangesAsync();
          }
          return TypedResults.Created($"/api/catalog/items/{productToUpdate.Id}");
-     }
+     }*/
 
-     public static async Task<Created> CreateItem(
-         [AsParameters] CatalogServices services,
-         CatalogItem product)
-     {
-         var item = new CatalogItem
-         {
-             Id = product.Id,
-             CatalogBrandId = product.CatalogBrandId,
-             CatalogTypeId = product.CatalogTypeId,
-             Description = product.Description,
-             Name = product.Name,
-             PictureFileName = product.PictureFileName,
-             Price = product.Price,
-             AvailableStock = product.AvailableStock,
-             RestockThreshold = product.RestockThreshold,
-             MaxStockThreshold = product.MaxStockThreshold
-         };
-         item.Embedding = await services.CatalogAI.GetEmbeddingAsync(item);
+    /*public static async Task<Created> CreateItem(
+        [AsParameters] CatalogServices services,
+        CatalogItem product)
+    {
+        var item = new CatalogItem
+        {
+            Id = product.Id,
+            CatalogBrandId = product.CatalogBrandId,
+            CatalogTypeId = product.CatalogTypeId,
+            Description = product.Description,
+            Name = product.Name,
+            PictureFileName = product.PictureFileName,
+            Price = product.Price,
+            AvailableStock = product.AvailableStock,
+            RestockThreshold = product.RestockThreshold,
+            MaxStockThreshold = product.MaxStockThreshold
+        };
+        item.Embedding = await services.CatalogAI.GetEmbeddingAsync(item);
 
-         services.Context.CatalogItems.Add(item);
-         await services.Context.SaveChangesAsync();
+        services.Context.CatalogItems.Add(item);
+        await services.Context.SaveChangesAsync();
 
-         return TypedResults.Created($"/api/catalog/items/{item.Id}");
-     }
+        return TypedResults.Created($"/api/catalog/items/{item.Id}");
+    }*/
 
-     public static async Task<Results<NoContent, NotFound>> DeleteItemById(
-         [AsParameters] CatalogServices services,
-         int id)
-     {
-         var item = services.Context.CatalogItems.SingleOrDefault(x => x.Id == id);
+    public static async Task<Results<NoContent, NotFound>> DeleteItemById(
+        [AsParameters] CatalogServices services,
+        int id)
+    {
+        var item = services.Context.CatalogItems.SingleOrDefault(x => x.Id == id);
 
-         if (item is null)
-         {
-             return TypedResults.NotFound();
-         }
+        if (item is null)
+        {
+            return TypedResults.NotFound();
+        }
 
-         services.Context.CatalogItems.Remove(item);
-         await services.Context.SaveChangesAsync();
-         return TypedResults.NoContent();
-     }
+        services.Context.CatalogItems.Remove(item);
+        await services.Context.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
 
-     private static string GetImageMimeTypeFromImageFileExtension(string extension) => extension switch
-     {
-         ".png" => "image/png",
-         ".gif" => "image/gif",
-         ".jpg" or ".jpeg" => "image/jpeg",
-         ".bmp" => "image/bmp",
-         ".tiff" => "image/tiff",
-         ".wmf" => "image/wmf",
-         ".jp2" => "image/jp2",
-         ".svg" => "image/svg+xml",
-         ".webp" => "image/webp",
-         _ => "application/octet-stream",
-     };
+    private static string GetImageMimeTypeFromImageFileExtension(string extension) => extension switch
+    {
+        ".png" => "image/png",
+        ".gif" => "image/gif",
+        ".jpg" or ".jpeg" => "image/jpeg",
+        ".bmp" => "image/bmp",
+        ".tiff" => "image/tiff",
+        ".wmf" => "image/wmf",
+        ".jp2" => "image/jp2",
+        ".svg" => "image/svg+xml",
+        ".webp" => "image/webp",
+        _ => "application/octet-stream",
+    };
 
-     public static string GetFullPath(string contentRootPath, string pictureFileName) =>
-         Path.Combine(contentRootPath, "Pics", pictureFileName);
-    */
+    public static string GetFullPath(string contentRootPath, string pictureFileName) =>
+        Path.Combine(contentRootPath, "Pics", pictureFileName);
+
 }
